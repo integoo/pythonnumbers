51dc1421db4585799a4afc7b865a055349945e69
integoo <eugalde72@gmail.com>
2024-10-31 18:20:41 -0700

    agregue sonarqube.sh y .gitignore

---
+++ sonarqube.sh
@@ -0,0 +1,5 @@
+/Users/eugalde/bin/sonar-scanner-6.2.1.4610-macosx-x64/bin/sonar-scanner \
+  -Dsonar.projectKey=pythonnumbers \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://10.0.0.101:9000 \
+  -Dsonar.token=sqp_78c3c9aaf2249c4af1cab1e8dfcb9bafd08ff7bb
